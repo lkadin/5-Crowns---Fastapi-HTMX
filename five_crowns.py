35d889cd6fc2e0f4f047d2c87775b4dcb5dcaa65
Lee Kadin <lkadin@gmail.com>
2025-09-09 14:22:27 -0400

    Change pick from deck to use card image

--- five_crowns.py
+++ five_crowns.py
@@ -285,7 +285,7 @@ class Game:
 
     def enable_all_actions(self):
         for self.action in self.actions:
-            if self.action.name not in ("Start", "Restart","Pick_from_discard"):
+            if self.action.name not in ("Start", "Restart","Pick_from_discard","Pick_from_deck"):
                 self.action.action_status = "enabled"
 
     def action_from_action_name(self, action_name: str) -> Action:
