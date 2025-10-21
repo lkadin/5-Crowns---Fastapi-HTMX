b043a7e12c14e88dc3fd6ccbb5b1764562bdfa3b
Lee Kadin <lkadin@gmail.com>
2025-10-21 10:41:17 -0400

    Additional coverage

--- tests/test_card.py
+++ tests/test_card.py
@@ -1,3 +1,6 @@
-def test_init(card):
+def test_init(card,deck):
     assert card.suit == "spade"
     assert card.rank == 3
+
+    for card in deck.cards:
+        assert card.suit in ["heart", "spade", "club", "diamond", "star","joker"]
