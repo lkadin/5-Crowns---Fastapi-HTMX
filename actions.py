87861648b5005a41433e864f3c4c9d3abe98b618
Lee Kadin <lkadin@gmail.com>
2025-11-12 14:03:06 -0500

    CHange description for Restart

--- actions.py
+++ actions.py
@@ -1,5 +1,5 @@
 actions_text = {
-    "Restart": "New game.",
+    "Restart": "Play again.",
     "Start": "Start game",
     "Pick_from_deck": "Pick a new card",
     "Pick_from_discard": "Pick a used card",
