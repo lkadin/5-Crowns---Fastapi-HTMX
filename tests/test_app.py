b75ce6c724f30841a60fa670136cb7262980133b
Lee Kadin <lkadin@gmail.com>
2025-11-12 08:06:37 -0500

    Add test_post_item

--- tests/test_app.py
+++ tests/test_app.py
@@ -16,4 +16,9 @@ def test_get_root(client):
 
 def test_read_item(client):
     response = client.get("/web/1/?user_name=Lee")
+    assert response.status_code == 405
+
+
+def test_post_item(client):
+    response = client.post("/web/1/", data={"user_name": "Lee"})
     assert response.status_code == 200
